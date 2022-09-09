cc95fa5641b30bf915036400a4ba9bfdf0936f38
elijahbeminiyi <elijahbeminiyi@gmail.com>
2022-09-09 17:31:54 +0100

    Alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - This function prints alphabets
+ * alphabets: stores all alphabets character
+ *
+ * Return: returns 0
+ */
+
+int main(void)
+{
+	/*Declarations*/
+	char alphabets;
+
+	/*Statements*/
+	for (alphabets = 'a'; alphabets <= 'z'; alphabets++)
+	{
+		putchar(alphabets);
+	}
+	return (0);
+}
