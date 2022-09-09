350cef3eefcdb04680e7c3ae53195b9245bb35f7
elijahbeminiyi <elijahbeminiyi@gmail.com>
2022-09-10 00:33:36 +0100

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - This function prints alphabets
+ * hex: stores all hexadecimal character
+ *
+ * Return: returns 0
+ */
+
+int main(void)
+{
+	/*Declarations*/
+	char letter;
+	int num;
+
+	/*Statements*/
+	for (num = 0; num <= 10; num++)
+	{
+		putchar((num) + '0');
+	}
+	for (letter = 'a'; letter <= 'f'; letter++)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}
