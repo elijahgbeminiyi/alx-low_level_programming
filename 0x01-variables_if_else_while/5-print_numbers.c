559f9bec814150858bc90450b20c7cac99e81eac
elijahbeminiyi <elijahbeminiyi@gmail.com>
2022-09-09 23:19:03 +0100

    5 added

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - This function prints alphabets
+ * num: stores all numbers
+ *
+ * Return: returns 0
+ */
+
+int main(void)
+{
+	/*Declarations*/
+	int i;
+
+	/*Statements*/
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+	return (0);
+}
