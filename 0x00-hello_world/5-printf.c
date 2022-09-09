63de351d0799f392f55444b82cbc3c322096c326
elijahgbeminiyi <elijahgbeminiyi@gmail.com>
2022-09-08 19:05:14 -0700

    New

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - This print a given output
+ * text: This be printed
+ *
+ * Return: returns an integer
+ */
+int main(void)
+{
+	/*declaration*/
+	char text[] = "with proper grammar, but the outcome is a piece of art";
+
+	/*statements*/
+	printf("%s,", text);
+	return (0);
+}
