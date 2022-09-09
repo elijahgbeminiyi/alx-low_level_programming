9dd99370b98d25d55a80f9aa6bcfeac87f700f95
elijahgbeminiyi <elijahgbeminiyi@gmail.com>
2022-09-08 17:40:32 -0700

    My first code in c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - This function will print a sentence.
+ * sent: This variable will contain a sentence passed into it
+ *
+ * Return: It returns 0
+ *
+ */
+
+int main(void)
+{
+	/*declarations*/
+	char sent[] = "\"Programming is like building a multilinual puzzle\n";
+
+	/*statement*/
+	puts(sent);
+	return (0);
+}
