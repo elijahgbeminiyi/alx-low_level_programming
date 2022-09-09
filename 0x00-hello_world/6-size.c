c8183ed47465bf0e5050e66f6943aebe83e66ae5
elijahgbeminiyi <elijahgbeminiyi@gmail.com>
2022-09-08 20:30:36 -0700

    God Abeg

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- *  main - This function prints all various data types
+ * main - This function prints all various data types
  *
  (* This programs is desined to print various data types and their sizes)
  *  Return: Returns 0
@@ -18,9 +18,10 @@ int main(void)
 	float e;
 
 	/*Statements*/
-	printf("Size of a char: %u byte(s)\n", (unsigned int)sizeof(a));
-	printf("Size of an int: %u byte(s)\n", (unsigned int)sizeof(b));
-	printf("Size of a long int: %lu byte(s)\n", (long unsigned int)sizeof(c));
-	printf("Size of a long long int: %llu byte(s)\n", (long long unsigned int)sizeof(d));
-	printf("Size of a float: %u byte(s)\n", (unsigned int)sizeof(e));
+	printf("Size of a char: %d byte(s)\n", sizeof(a));
+	printf("Size of an int: %d byte(s)\n", sizeof(b));
+	printf("Size of a long int: %d byte(s)\n", sizeof(c));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(d));
+	printf("Size of a float: %d byte(s)\n", sizeof(e));
+	return (0);
 }
