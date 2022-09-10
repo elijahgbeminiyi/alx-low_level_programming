a3a5e09f05b4a87ab5a67396d0e24c0fc2aa6b03
elijahbeminiyi <elijahbeminiyi@gmail.com>
2022-09-10 01:56:56 +0100

    Combo brain

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,13 +13,13 @@ int main(void)
 	int num, num2;
 
 	/*Statements*/
-	for (num = 0; num < 10; num++)
+	for (num = 0; num < 9; num++)
 	{
-		for (num2 = 0; num2 < 10; num2++)
+		for (num2 = num +1; num2 < 10; num2++)
 		{
 			putchar((num % 10) + '0');
 			putchar((num2 % 10) + '0');
-			if (num == '8' && num2 == '9')
+			if (num == 8 && num2 == 9)
 			{
 				continue;
 			}
