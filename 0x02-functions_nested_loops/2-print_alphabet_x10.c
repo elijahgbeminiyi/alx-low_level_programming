b292b5ff28951f75d6e4f624357d988c0f1bf855
elijahbeminiyi <elijahbeminiyi@gmail.com>
2022-09-13 23:12:13 +0100

    Grust

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet 10 times
+ *
+ * Return: return 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		print_alphabet();
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
