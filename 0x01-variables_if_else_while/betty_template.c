9938e6a8e679184300a7370b53b33e5a60f7bd4a
elijahbeminiyi <elijahbeminiyi@gmail.com>
2022-09-13 13:31:39 +0100

    Sweet

---
+++ 0x01-variables_if_else_while/betty_template.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - This function prints alphabets
+ * num: stores all numbers
+ *
+ * Return: returns 0
+ */
+
+int main(void)
+{
+	/*Declarations*/
+	
+
+	/*Statements*/
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - This functions print a simple string
+ *
+ * Return: it returns 0
+ */
+
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
+}
