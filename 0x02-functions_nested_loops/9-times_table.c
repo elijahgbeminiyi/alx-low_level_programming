783c567fa0a1e6066856649d9a04989bb85f3a15
elijahbeminiyi <elijahbeminiyi@gmail.com>
2022-09-14 02:57:03 +0100

    loike

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * times_table - description here
+ * i: saves interger 1
+ * j: saves integer 2
+ *
+ * Return: returns 0
+ */
+
+void times_table(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			int mul = i * j;
+
+			if (j == 0)
+			{
+				_putchar('0);
+			}
+			else if (mul < 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(mul + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(mul / 10 + '0');
+				_putchar(mul % 10 + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
+
