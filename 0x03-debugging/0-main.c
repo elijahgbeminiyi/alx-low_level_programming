f3fce2edbb9995548878356f72c3e289f3e0ef24
elijahbeminiyi <elijahbeminiyi@gmail.com>
2022-09-14 12:58:26 +0100

    Begining

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - tests function that print if integer is
+ * 	positive or negative
+ * i: takes the integer to be tested
+ *
+ * Return: Returns 0
+ */
+
+int main(void)
+{
+	/*Initializing the variable*/
+	int i;
+
+	/*Statements*/
+
+	i = 0; /*Assigning a value to the variable*/
+	positive_or_negative(i); /*Calling the positive or negative function*/
+
+	return (0);
+}
