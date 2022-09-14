410d03564cea09e3245be071329db18143ddc86f
elijahbeminiyi <elijahbeminiyi@gmail.com>
2022-09-14 03:07:22 +0100

    ihshoid

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_to_98 - adds two numbers
+ * @n1: first integer
+ * @n2: second integer
+ *
+ * Return: returns the addition
+ */
+
+int add(int n1, int n2)
+{
+	int res = n1 + n2;
+
+	return (res);
+}
