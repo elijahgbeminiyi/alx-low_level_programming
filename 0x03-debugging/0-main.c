88c967cff13d0c5ea7593581366d39b4f44fe9b2
elijahbeminiyi <elijahbeminiyi@gmail.com>
2022-09-14 13:00:43 +0100

    wahala1

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * main - tests function that print if integer is
- * 	positive or negative
+ * main - tests function that print if integer is positive or negative
+ *
  * i: takes the integer to be tested
  *
  * Return: Returns 0
@@ -14,7 +14,6 @@ int main(void)
 	int i;
 
 	/*Statements*/
-
 	i = 0; /*Assigning a value to the variable*/
 	positive_or_negative(i); /*Calling the positive or negative function*/
 
