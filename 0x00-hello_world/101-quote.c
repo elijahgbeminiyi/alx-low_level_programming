603deb4c7fc6a8e0924c7902f82ec3088f7d1215
elijahbeminiyi <elijahbeminiyi@gmail.com>
2022-09-14 01:56:02 +0100

    hsdifh

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,2 @@
+#include <stderr.h>
+
---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _abs - Decription o
+ * @c: sets the number
+ *
+ * Return: returns 0
+ */
+
+int _abs(int c)
+{
+	if (c > 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (c * -1);
+	}
+}
