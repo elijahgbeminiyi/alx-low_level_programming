1aba8af30d879eb1c7f0413f1a392e8773572b8e
elijahgbeminiyi <elijahgbeminiyi@gmail.com>
2022-09-13 20:12:40 -0700

    Free gidt

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _main - writes the character c to stdout
+ * main - writes the character c to stdout
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
