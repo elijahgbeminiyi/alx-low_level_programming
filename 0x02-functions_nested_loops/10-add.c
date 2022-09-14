8a2cec62e34a1a44ea9ec4912776f4bb4017245a
elijahbeminiyi <elijahbeminiyi@gmail.com>
2022-09-14 03:04:13 +0100

    shdi

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,8 +2,8 @@
 
 /**
  * add - adds two numbers
- * n1: first integer
- * n2: second integer
+ * @n1: first integer
+ * @n2: second integer
  *
  * Return: returns the addition
  */
@@ -11,5 +11,6 @@
 int add(int n1, int n2)
 {
 	int res = n1 + n2;
+
 	return (res);
 }
