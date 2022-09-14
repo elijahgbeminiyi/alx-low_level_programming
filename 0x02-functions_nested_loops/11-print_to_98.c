942e5171972ad5b965eeb23209f8166442d30a18
elijahgbeminiyi <elijahgbeminiyi@gmail.com>
2022-09-13 19:44:46 -0700

    dry hard

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,15 +2,30 @@
 
 /**
  * print_to_98 - adds two numbers
- * @n1: first integer
- * @n2: second integer
+ * @n: takes the number 
  *
  * Return: returns the addition
  */
 
-int add(int n1, int n2)
+int print_to_98(int n)
 {
-	int res = n1 + n2;
-
-	return (res);
+	while (n > 98)
+	{
+		_putchar(n + '0');
+		_putchar(',');
+		_putchar(' ');
+		n--;
+	}
+	while (n < 98)
+	{
+		_putchar(n + '0');
+		_putchar(',');
+		_putchar(' ');
+		n++;
+	}
+	if (n == 98)
+	{
+		_putchar(n + '0');
+	}
+	return (0);
 }
