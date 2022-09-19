963591cc687fe38fe5969314199f05499258b6b3
elijahbeminiyi <elijahbeminiyi@gmail.com>
2022-09-19 22:52:57 +0100

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _puts - Prints a string
+ * @str: parameter string
+ *
+ * Return: Function is void
+ */
+
+void _puts(char *str)
+{
+	int i;
+	int len = _strlen(*str);
+
+	for (i = 0; i <= len; i++)
+	{
+		if (str[i] == '\0')
+		{
+			_putchar('\n');
+		}
+		_putchar(str[i]);
+	}
+}
