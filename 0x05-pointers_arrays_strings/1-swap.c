7c6cd1d3dc381f67ff1640b1d988b872862b41aa
elijahbeminiyi <elijahbeminiyi@gmail.com>
2022-09-19 22:10:49 +0100

    making a swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int - makes a swap
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: function is void
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = b;
+	*b = a;
+}
