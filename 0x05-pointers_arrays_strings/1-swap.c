7e1e4b8fac71faa24dfdadb010b3a651ec018c39
elijahbeminiyi <elijahbeminiyi@gmail.com>
2022-09-19 22:15:52 +0100

    swap o

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -4,12 +4,16 @@
  * swap_int - makes a swap
  * @a: first integer
  * @b: second integer
+ * swap: defines the swap variable
  *
  * Return: function is void
  */
 
 void swap_int(int *a, int *b)
 {
+	int swap;
+
+	swap = *a
 	*a = *b;
-	*b = *a;
+	*b = swap;
 }
