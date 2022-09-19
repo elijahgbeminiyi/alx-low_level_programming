9a1ba78cc01e2078ef90517d0d96b788d79d982d
elijahbeminiyi <elijahbeminiyi@gmail.com>
2022-09-19 23:14:21 +0100

    Revverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev - Prints reverse string
+ * @s: parameter variable
+ *
+ * Return: A void function
+ */
+
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = 0; i <= 0; i--)
+	{
+		if (s[i] == '\0')
+		{
+			_putchar('\n');
+			break;
+		}
+		_putchar(s[i]);
+	}
+}
