9e846a16e56ae1a2b378f45429aaa269de43d9a0
elijahgbeminiyi <elijahgbeminiyi@gmail.com>
2022-09-22 05:11:59 -0700

    Dream

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * _strcmp - This funtion compares two strings
@@ -11,11 +12,20 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0, diff;
+	int i = 0, diff = 0;
 
-	for (; (s1[i] != '\0') && (s2[i] != '\0'); i++)
+	for (i = 0; ((s1[i] != '\0') && (s2[i] != '\0')); i++)
 	{
 		diff = s1[i] - s2[i];
 	}
 	return (diff);
+}
+int main(void)
+{
+	char s1[] = "Hello";
+	char s2[] = "World!";
+	printf("%d\n", _strcmp(s1, s2));
+	printf("%d\n", _strcmp(s2, s1));
+	printf("%d\n", _strcmp(s1, s1));
+	return (0);
 }
