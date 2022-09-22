628b2426c690df7262dda3c82fda96201906644c
elijahgbeminiyi <elijahgbeminiyi@gmail.com>
2022-09-22 04:49:28 -0700

    Compare strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcmp - This funtion compares two strings
+ * @s1: first param
+ * @s2: second param
+ *
+ * Return: returns the difference
+ *
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0, diff;
+
+	for (; (s1[i] != '\0') && (s2[i] != '\0'); i++)
+	{
+		diff = s1[i] - s2[i];
+	}
+	return (diff);
+}
