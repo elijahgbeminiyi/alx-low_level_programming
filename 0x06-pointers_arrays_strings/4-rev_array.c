0500c6c0581a130078f1999b3e9b85b988d0f681
elijahbeminiyi <elijahbeminiyi@gmail.com>
2022-09-22 22:22:40 +0100

    Reversed array implemented

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -8,22 +8,15 @@
 
 void reverse_array(int *a, int n)
 {
-	int *p, i, aux, k;
+	int i, j, temp;
 
-	/* Let's assume a = [1, 2, 3, 4] */
-	p = a;	/* Assigning the address of the first element to  p */
-
-	for (i = 0; i < n; i++)	/* Looping through the lenght of the array */
+	for (i = 0; i < n - 1; i++)
 	{
-		p++;
-	}
-	/* At this point, p holds the address of the last element */
-
-	for (k = 0; k < i / 2; k++)
-	{
-		aux = a[k];	/*Creating an aux array with the value of a[0],i.e aux=[1]*/
-		a[k] = *p;	/*then, the first elmt a[0] now holds the val of the lst elmt*/
-		*p = aux;	/*the val of the lst elmt is now asgned to the aux arry*/
-		p--;		/*...the address is decremented, hence the swap is made*/
+		for (j = i + 1; j > 0; j--)
+		{
+			temp = a[j];
+			a[j] = a[j - 1];
+			a[j - 1] = temp;
+		}
 	}
 }
