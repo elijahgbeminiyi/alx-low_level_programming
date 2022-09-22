7c5c25a49dbffd38614445bf14d0dce78f9756c6
elijahbeminiyi <elijahbeminiyi@gmail.com>
2022-09-22 22:30:09 +0100

    Toupper string implemented

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* string_toupper - This function changes all lowercase to uppercase
+* @s: string parameter
+* Return: Returns a string.
+*/
+
+char *string_toupper(char *s)
+{
+	int count = 0;
+
+	while (*(s + count) != '\0')
+	{
+		if ((*(s + count) >= 97) && (*(s + count) <= 122))
+			*(s + count) = *(s + count) - 32;
+		count++;
+	}
+
+	return (s);
+}
