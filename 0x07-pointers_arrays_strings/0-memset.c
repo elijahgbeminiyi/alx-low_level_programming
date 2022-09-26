01ae3b02675f399f4a0283ba420b958ed64ebbd7
elijahbeminiyi <elijahbeminiyi@gmail.com>
2022-09-26 12:15:20 +0100

    hot fix

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
 * _memset - This fuction is a memory set
@@ -11,11 +12,6 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i;
-
-	for (i = 0; i < n; i++)
-	{
-		s[i] = b;
-	}
-	return (*s);
+	memset(s, b, n);
+	return (s);
 }
