f338aa899012a2f03c7ea927ebf5c540a82114d7
elijahbeminiyi <elijahbeminiyi@gmail.com>
2022-09-26 12:29:35 +0100

    try these

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* _memset - This fuction is a memory set
+* @s: string param
+* @b: character param
+* @n: integer param
+*
+* Return: Returns a string
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	memset(s, b, n);
+	return (s);
+}
