690ae06a9d360ae707614dad7c438cfc45f6bd6a
elijahbeminiyi <elijahbeminiyi@gmail.com>
2022-09-26 16:01:03 +0100

    Free me

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,7 +1,7 @@
 #ifndef _MAIN_H
 #define _MAIN_H
 
-int putchar(char c);
+int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 
