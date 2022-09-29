8641c33528a559fd016cf3cf8e6a66978270bd02
elijahbeminiyi <elijahbeminiyi@gmail.com>
2022-09-29 01:03:06 +0100

    Comments added to codes

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -20,24 +20,23 @@ int factorial(int n)
 	}
 	return (n * factorial(n - 1));
 }
-	/**
-	* CALL STACK
-	* if n = 3
-	* the first two if statements aren't satisfied,
-	* so it returns (n * function(n - 1)), which is
-	* return (3 * function(2))
-	*
-	* function(2) is called now,
-	* it still didn't satify the conditions
-	* therefore it returns (n * function(n - 1)), which is
-	* return (2 * function(1))
-	* on the stack frame we have...
-	* return (3 * 2 * function(1))
-	*
-	* function(1) is called now
-	* it meets a condition that returns (1)
-	* we now have...
-	* return (3 * 2 * 1), on the stack frame.
-	* we simple get 6 as the output.
-	*/
-
+/**
+* CALL STACK
+* if n = 3
+* the first two if statements aren't satisfied,
+* so it returns (n * function(n - 1)), which is
+* return (3 * function(2))
+*
+* function(2) is called now,
+* it still didn't satify the conditions
+* therefore it returns (n * function(n - 1)), which is
+* return (2 * function(1))
+* on the stack frame we have...
+* return (3 * 2 * function(1))
+*
+* function(1) is called now
+* it meets a condition that returns (1)
+* we now have...
+* return (3 * 2 * 1), on the stack frame.
+* we simple get 6 as the output.
+*/
