1992a525400be9b1a1a2358ce571e76e027f7e24
elijahbeminiyi <elijahbeminiyi@gmail.com>
2022-10-04 00:05:15 +0100

    trial

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+* str_concat - function concatenates two strings
+* @s1: first string parameter
+* @s2: second string parameter
+* Return: returns the concatenated string
+*/
+
+char *str_concat(char *s1, char *s2)
+{
+	char *new;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	new = malloc(sizeof(s1) + sizeof(s2) + 1);
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	new[] = s1 + s2;
+	return (new);
+}
