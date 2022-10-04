534bb26072ba62d3af959015b645c7ba810bc8e9
elijahbeminiyi <elijahbeminiyi@gmail.com>
2022-10-04 22:29:27 +0100

    Concatenation implemented

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -18,9 +18,9 @@ char *str_concat(char *s1, char *s2)
 	i = 0;
 	j = 0;
 	if (s1 == NULL)
-		return ("");
+		s1 = "";
 	else if (s2 == NULL)
-		return ("");
+		s2 = "";
 	while (s1[count_1])
 	{
 		count_1++;
