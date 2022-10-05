8d42f9fe9587965d20baa19f9ef2c80e6c5b5fae
elijahbeminiyi <elijahbeminiyi@gmail.com>
2022-10-05 01:12:52 +0100

    Freedom of life

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+* free_grid - function frees allocated grid
+* @grid: grid parameter
+* @height: integer parameter
+*/
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	i = 0;
+	while (i < height)
+	{
+		free(grid[i]);
+		i++
+	}
+	free(grid);
+}
