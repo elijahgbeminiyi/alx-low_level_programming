6529a9c16429d9ef4f434cc243fed950a83a6c6c
elijahbeminiyi <elijahbeminiyi@gmail.com>
2022-10-11 02:28:32 +0100

    greetins

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,22 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+* print_dog - checks a null element
+* @d: address of dog
+*/
+
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		if (d->name == NULL)
+		{
+			printf("Name: (nil)\n");
+		}
+	}
+	else
+	{
+		printf("\n")
+	}
+}
