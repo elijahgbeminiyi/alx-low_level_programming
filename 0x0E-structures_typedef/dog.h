701f0f4f6dab7caa3b0cc0b4d94d7d3561206ab1
elijahbeminiyi <elijahbeminiyi@gmail.com>
2022-10-11 02:11:15 +0100

    God abeg

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,7 +2,7 @@
 #define _DOG_H_
 
 /**
-* dog - a struct function
+* struct dog - structure of charity
 * @name: name of dog
 * @age: age of dog
 * @owner: owner of dog
@@ -14,5 +14,6 @@ struct dog
 	float age;
 	char *owner;
 };
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif
