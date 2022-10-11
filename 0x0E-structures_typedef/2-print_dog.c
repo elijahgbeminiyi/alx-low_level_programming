5846139dbc76660ff91bb3206bcccc1109848396
elijahbeminiyi <elijahbeminiyi@gmail.com>
2022-10-11 02:56:49 +0100

    be carful

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,32 +9,29 @@
 
 void print_dog(struct dog *d)
 {
-	if (d != NULL)
+	if (d->name == NULL)
 	{
-		if (d->name == NULL)
-		{
-			printf("Name: (nil)\n");
-		}
-		else
-		{
-			printf("Name: %s\n", d->name)
-		}
-		if (d->age <= 0)
-		{
-			printf("Age: (nil)\n");
-		}
-		else
-		{
-			printf("Age: %d\n", d->age);
-		}
-		if (d->owner == NULL)
-		{
-			printf("Owner: (nil)\n");
-		}
-		else
-		{
-			print("Owner: %s\n", d->owner);
-		}
+		printf("Name: (nil)\n");
+	}
+	else
+	{
+		printf("Name: %s\n", d->name)
+	}
+	if (d->age < 0)
+	{
+		printf("Age: (nil)\n");
+	}
+	else
+	{
+		printf("Age: %d\n", d->age);
+	}
+	if (d->owner == NULL)
+	{
+		printf("Owner: (nil)\n");
+	}
+	else
+	{
+		print("Owner: %s\n", d->owner);
 	}
 	if (d == NULL)
 	{
