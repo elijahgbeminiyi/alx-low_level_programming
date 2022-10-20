fc80702adefd85370daf389e8a0cbcdf1586825f
elijahbeminiyi <elijahbeminiyi@gmail.com>
2022-10-20 22:45:40 +0100

    sio

---
+++ 0x12-singly_linked_lists/0-print_lists.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* print_list - prints the elements of a list
+* @h: character parameters
+*/
+
+size_t print_list(const list_t *h)
+{
+	int count = 0;
+
+	if (h = NULL)
+		printf("[%d] (nil)\n", count);
+	list_t *ptr = NULL;
+	ptr = h;
+	while (ptr != NULL)
+	{
+		count++;
+		ptr = ptr->next;
+	}
+	return(count);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,2 @@
+**Singly Linked Lists**
+This consists of tasks associated with linked lists in C programming language
