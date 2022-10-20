e30def5ddbd9ab8f30cf5e6cc8f76995fe3a2b30
elijahbeminiyi <elijahbeminiyi@gmail.com>
2022-10-21 00:17:30 +0100

    ksd

--- 0x12-singly_linked_lists/0-print_lists.c
+++ 0x12-singly_linked_lists/0-print_lists.c
@@ -13,11 +13,11 @@ size_t print_list(const list_t *h)
 	while (h != NULL)
 	{
 		if (h->str == NULL)
-			printf("[%d] (nil)\n", count)
+			printf("[%d] %s\n", 0, "(nil)");
 		else
-			printf("[%d] %s\n", h->len, h->str)
+			printf("[%i] %s\n", h->len, h->str);
+			h = h->next;
 			count++;
-		h = h->next;
 	}
 	return (count);
 }
