0b931b58df3b1395d99e680dd873b8da86c592c2
elijahbeminiyi <elijahbeminiyi@gmail.com>
2022-10-21 00:52:50 +0100

    livers

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -9,17 +9,11 @@
 size_t list_len(const list_t *h)
 {
 	size_t count = 0;
-	list_t *ptr;
 
-	ptr = malloc(sizeof(list_t);
-	ptr = h;
 	while (ptr != NULL)
 	{
-		if (ptr->next == NULL)
-			return(count);
-		else
-			ptr = ptr->next;
-			count++;
+		ptr = ptr->next;
+		count++;
 	}
 	return (count);
 }
